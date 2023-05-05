c465128ed551b6a2e87a308d91647d104226d8bf
Martin Münch <advocd@gmail.com>
2023-05-05 08:08:29 +0200

    moved slides

--- work_bench/main.py
+++ work_bench/main.py
@@ -1,5 +1,14 @@
-from microbit import pin0, sleep
+from microbit import display, Image, pin0
 
 while True:
-    print(pin0.read_analog())
-    sleep(500)
+    """
+    hier ist auch pin0.read_digital() möglich, 
+    dann ist der Wert aber 0 und 1 (Int) und nicht 
+    True oder False
+    """
+    if pin0.is_touched():
+        display.show(Image.HEART)
+        print("touched")
+    else:
+        display.show(Image.HEART_SMALL)
+        print('not touched')
